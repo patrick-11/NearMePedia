978d70754c8c3a92484e9e7d09654d8c351116ac
Patrick <29860751+patrick-11@users.noreply.github.com>
2020-02-17 20:04:35 +0100

    Added Activity Loader

--- App/components/ArticleList.js
+++ App/components/ArticleList.js
@@ -1,29 +1,36 @@
-import React, { useEffect } from "react";
-import {Text, View, FlatList} from "react-native";
+import React from "react";
+import {Text, View, FlatList, ActivityIndicator} from "react-native";
 
 import Article from "./Article";
+import style from "../style/style"
 
 
 const ArticleList = (props) => {
     return (
         <View style = {{flex: 1}}>
-            {props.articles ?
-                <FlatList
-                    data = {props.articles}
-                    keyExtractor = {(_, index) => index.toString()}
-                    renderItem = {(article) => (
-                        <Article
-                            screen = {props.screen}
-                            article = {article}
-                            containerSaved = {props.containerSaved}
-                        />
-                    )}
-                />
+            {props.articles.length > 0 ?
+                props.containerArticle.getLoading() ?
+                    <ActivityIndicator style = {style.loader}/>
+                    :
+                    <FlatList
+                        data = {props.articles}
+                        keyExtractor = {(_, index) => index.toString()}
+                        renderItem = {(article) => (
+                            <Article
+                                screen = {props.screen}
+                                article = {article}
+                                containerSaved = {props.containerSaved}
+                            />
+                        )}
+                    />
             :
-                props.screen ?
+            <View style = {style.center}>
+                {props.screen ?
                     <Text>Select a Location!</Text>
                 :
                     <Text>Save an Article!</Text>
+                }
+            </View>
             }
         </View>
     );
